e2ab2a0e8d523a49ca3e10c6d0df9338d61b23e1
Manuel Tobío Aldao <teymanu@gmail.com>
2020-12-08 23:43:07 +0100

    .

--- lib/resources/utils/Constants.dart
+++ lib/resources/utils/Constants.dart
@@ -1,10 +1,10 @@
 // Redirect URI of the microsoft app
-const String MICROSOFT_OAUTH_REDIRECT_URI = "https://www.imatia.com/employees-portal/";
+const String MICROSOFT_OAUTH_REDIRECT_URI = "";
 // Microsoft application id
-const String MICROSOFT_AZURE_APP_ID = "c595fec4-8b25-4ee8-aa97-c1e05aef0676";
+const String MICROSOFT_AZURE_APP_ID = "";
 
 // Server REST URL
-const String SERVER_REST_URL = "https://elastic-erp.imatia.com/elastic-imatia-employees-portal-server-wsrest";
+const String SERVER_REST_URL = "";
 
 // REST services endpoints
 const String REST_SERVICE_PATH_LOGIN_EMPLOYEE = "/loginEmployees";
@@ -12,7 +12,6 @@ const String REST_SERVICE_PATH_GET_EMPLOYEE_ID_FROM_THE_USER_MAIL = "/getEmploye
 const String REST_SERVICE_PATH_INSERT_CONTROL_HOURS = "/insertControlHours";
 const String REST_SERVICE_PATH_START_TIMING = "/startTiming";
 const String REST_SERVICE_PATH_STOP_TIMING = "/stopTiming";
-
 
 // Entities
 const String ENTITY_EMPLOYEE_PRESENCE_CONTROL_HOURS = "EMobLAEmpleadosPresenceControlHours";
