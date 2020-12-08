e54200a1b077ed34e5456c2276312ece3a6ac213
Manuel Tobío Aldao <teymanu@gmail.com>
2020-12-08 23:41:13 +0100

    fixed annaul page

--- lib/resources/utils/Constants.dart
+++ lib/resources/utils/Constants.dart
@@ -1,20 +1,19 @@
 // Redirect URI of the microsoft app
-const String MICROSOFT_OAUTH_REDIRECT_URI = "";
+const String MICROSOFT_OAUTH_REDIRECT_URI = "https://www.imatia.com/employees-portal/";
 // Microsoft application id
-const String MICROSOFT_AZURE_APP_ID = "";
+const String MICROSOFT_AZURE_APP_ID = "c595fec4-8b25-4ee8-aa97-c1e05aef0676";
 
 // Server REST URL
-const String SERVER_REST_URL = "";
+const String SERVER_REST_URL = "https://elastic-erp.imatia.com/elastic-imatia-employees-portal-server-wsrest";
 
 // REST services endpoints
-const String REST_SERVICE_PATH_LOGIN_EMPLOYEE = "";
-const String REST_SERVICE_PATH_GET_EMPLOYEE_ID_FROM_THE_USER_MAIL = "";
-const String REST_SERVICE_PATH_INSERT_CONTROL_HOURS = "";
-const String REST_SERVICE_PATH_START_TIMING = "";
-const String REST_SERVICE_PATH_STOP_TIMING = "";
+const String REST_SERVICE_PATH_LOGIN_EMPLOYEE = "/loginEmployees";
+const String REST_SERVICE_PATH_GET_EMPLOYEE_ID_FROM_THE_USER_MAIL = "/getEmployeeIdFromTheUserMail";
+const String REST_SERVICE_PATH_INSERT_CONTROL_HOURS = "/insertControlHours";
+const String REST_SERVICE_PATH_START_TIMING = "/startTiming";
+const String REST_SERVICE_PATH_STOP_TIMING = "/stopTiming";
 
 
 // Entities
-const String ENTITY_EMPLOYEE_PRESENCE_CONTROL_HOURS = "";
-const String ENTITY_EMPLOYEE_PRESENCE_CONTROL_MONTH_HOURS = ""; 
-
+const String ENTITY_EMPLOYEE_PRESENCE_CONTROL_HOURS = "EMobLAEmpleadosPresenceControlHours";
+const String ENTITY_EMPLOYEE_PRESENCE_CONTROL_MONTH_HOURS = "EMobLAEmpleadosPresenceControlMonthHours";
